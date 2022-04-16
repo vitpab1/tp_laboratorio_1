09f487c00b56d29bfb4910a6baeb3be797118a44
Pablo Vittar <vitpablo1@gmail.com>
2022-04-16 11:43:57 -0300

    asd

---
+++ src/TP1.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : TP1.c
+ Author      : Pablo
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	int num1;
+	int num2;
+	int suma;
+	printf("ingrese un numero\n");
+	scanf("%d", &num1);
+	printf("ingrese un numero\n");
+	scanf("%d", &num2);
+	suma = num1 + num2;
+	printf("suma es %d", suma);
+
+	return EXIT_SUCCESS;
+}
