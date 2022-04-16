c2ccbba206d5c6099bd4d8c6ddad408ec709c377
Pablo Vittar <vitpablo1@gmail.com>
2022-04-16 23:52:00 +0300

    fix calc

--- src/input.h
+++ src/input.h
@@ -9,11 +9,30 @@
 #define INPUT_H_
 
 int suma(int a, int b);
-
+///
 int utn_getNumero(int* pResultado,char* mensaje,char* mensajeError,int minimo,int maximo,int reintentos);
-int utn_getNumeroFloat(float* pResultado,char* mensaje,char* mensajeError,float minimo,float maximo,int reintentos);
-float calculoTotales(int km, float latam, float aerolineas);
+float utn_getNumeroFloat(float* pResultado,char* mensaje,char* mensajeError,float minimo,float maximo,int reintentos);
 
-
+/// @param precio Toma el precio y lo multiplica por 0.9
+/// @return
+float calculoDebito(float precio);
+/// @param precio  Toma el precio y lo multiplica por 1.25
+/// @return
+float calculoCredito(float precio);
+/// @param precio  Toma el precio y lo divide por el precio del bitcoin
+/// @return
+float calculoBitcoin(float precio) ;
+/// @param precio Toma el precio y lo divida por la cantidad de km
+/// @param km
+/// @return
+float calculoUnitario(float precio, float km);
+/// @param precioLatam
+/// @param precioAero
+/// @return muestra el valor absoluto de la resta del precio de latam y aerolineas
+float diferenciaPrecio (float precioLatam, float precioAero);
+/// @param mensaje
+/// @param valor
+/// @return
+float valorMostrar (char *mensaje, float valor);
 
 #endif /* INPUT_H_ */
