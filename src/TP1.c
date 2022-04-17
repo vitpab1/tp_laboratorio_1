5e21109bc3de081f17fab80b8afc0bc5b6e9a703
Pablo Vittar <vitpablo1@gmail.com>
2022-04-16 23:57:35 -0300

    fix calc

--- src/TP1.c
+++ src/TP1.c
@@ -21,35 +21,119 @@ int main(void) {
 	float precioAerolineas;
 	float precioLatam;
 
+	float latamDebito;
+	float latamCredito;
+	float latamBitcoin;
+	float latamUnitario;
 
+	float aeroDebito;
+	float aeroCredito;
+	float aeroBitcoin;
+	float aeroUnitario;
 
+	float diferencia;
 
 	do {
 		respuesta =
 				utn_getNumero(&opcion,
 						"Selecciones una opcion \n\n1-Ingresar Kilómetros: \n2-Ingresar Precio de Vuelos: \n3-Calcular todos los costos: \n4-Informar Resultados: \n5-Carga forzada de datos:  \n6-Salir: \n",
-						"No es una opcion valida", 1, 3, 2);
+						"No es una opcion valida", 1, 6, 2);
 		if (!respuesta) {
 			switch (opcion) {
 			case 1:
-			kmIngresados =	utn_getNumeroFloat(&kmIngresados, "Ingresar kms", "Error. Tiene que ser un numero entre 50 y 20000", 50, 20000,99999);
+				kmIngresados = utn_getNumeroFloat(&kmIngresados, "Ingresar kms",
+						"Error. Tiene que ser un numero entre 50 y 20000", 50,
+						20000, 99999);
 				break;
 			case 2:
-				precioAerolineas =	utn_getNumeroFloat(&precioAerolineas, "Ingresar Precio Aerolineas", "Error. Tiene que ser un numero entre $1 y $500000", 1, 500000,99999);
-				precioLatam =	utn_getNumeroFloat(&precioLatam, "Ingresar Precio Latam", "Error. Tiene que ser un numero entre $1 y $500000", 1, 500000,99999);
+				if (kmIngresados != 0) {
+					precioAerolineas = utn_getNumeroFloat(&precioAerolineas,
+							"Ingresar Precio Aerolineas",
+							"Error. Tiene que ser un numero entre $1 y $500000",
+							1, 500000, 99999);
+					precioLatam = utn_getNumeroFloat(&precioLatam,
+							"Ingresar Precio Latam",
+							"Error. Tiene que ser un numero entre $1 y $500000",
+							1, 500000, 99999);
+				} else {
+
+					printf("Error. Ingrese los kms para continuar");
+				}
 
 				break;
 			case 3:
-				printf("entre al caso 1\n");
+				if (kmIngresados > 0 && precioAerolineas > 0
+						&& precioLatam > 0) {
+					latamDebito = calculoDebito(precioLatam);
+					latamCredito = calculoCredito(precioLatam);
+					latamBitcoin = calculoBitcoin(precioLatam);
+					latamUnitario = calculoUnitario(precioLatam, kmIngresados);
+					aeroDebito = calculoDebito(precioAerolineas);
+					aeroCredito = calculoCredito(precioAerolineas);
+					aeroBitcoin = calculoBitcoin(precioAerolineas);
+					aeroUnitario = calculoUnitario(precioAerolineas,
+							kmIngresados);
+					diferencia = diferenciaPrecio(precioLatam,
+							precioAerolineas);
+
+					printf("Calculos realizados");
+				} else {
+					printf("Error. Ingrese los kms y precios para continuar");
+
+				}
+
 				break;
 			case 4:
-				printf("entre al caso 2\n");
+
+				valorMostrar("Kms Ingresados: ", kmIngresados);
+
+				valorMostrar("Precio Latam: ", precioLatam);
+
+				valorMostrar("a)Precio con tarjeta de débito: ", latamDebito);
+				valorMostrar("b)Precio con tarjeta de credito: ", latamCredito);
+				valorMostrar("c)Precio con Bitcoin de débito: ", latamBitcoin);
+				valorMostrar("d)Precio Unitario: ", latamUnitario);
+				printf("\n");
+				valorMostrar("Precio Latam: ", precioAerolineas);
+				valorMostrar("a)Precio con tarjeta de débito: ", aeroDebito);
+				valorMostrar("b)Precio con tarjeta de credito: ", aeroCredito);
+				valorMostrar("c)Precio con Bitcoin de débito: ", aeroBitcoin);
+				valorMostrar("d)Precio Unitario: ", aeroUnitario);
+
+				valorMostrar("La diferencia de precio es: ", diferencia);
+
 				break;
 			case 5:
-				printf("entre al caso 1\n");
+				kmIngresados = 7090;
+				precioAerolineas = 162965;
+				precioLatam = 159339;
+				latamDebito = calculoDebito(precioLatam);
+				latamCredito = calculoCredito(precioLatam);
+				latamBitcoin = calculoBitcoin(precioLatam);
+				latamUnitario = calculoUnitario(precioLatam, kmIngresados);
+				aeroDebito = calculoDebito(precioAerolineas);
+				aeroCredito = calculoCredito(precioAerolineas);
+				aeroBitcoin = calculoBitcoin(precioAerolineas);
+				aeroUnitario = calculoUnitario(precioAerolineas, kmIngresados);
+				diferencia = diferenciaPrecio(precioLatam, precioAerolineas);
+				valorMostrar("Kms Ingresados: ", kmIngresados);
+				printf("\n");
+				valorMostrar("Precio Latam: ", precioLatam);
+				valorMostrar("a)Precio con tarjeta de débito: ", latamDebito);
+				valorMostrar("b)Precio con tarjeta de credito: ", latamCredito);
+				valorMostrar("c)Precio con Bitcoin de débito: ", latamBitcoin);
+				valorMostrar("d)Precio Unitario: ", latamUnitario);
+				printf("\n");
+				valorMostrar("Precio Latam: ", precioAerolineas);
+				valorMostrar("a)Precio con tarjeta de débito: ", aeroDebito);
+				valorMostrar("b)Precio con tarjeta de credito: ", aeroCredito);
+				valorMostrar("c)Precio con Bitcoin de débito: ", aeroBitcoin);
+				valorMostrar("d)Precio Unitario: ", aeroUnitario);
+				printf("\n");
+
+				valorMostrar("La diferencia de precio es: ", diferencia);
 				break;
 			case 6:
-				printf("entre al caso 2\n");
 				break;
 
 			}
--- src/input.c
+++ src/input.c
@@ -13,34 +13,50 @@ int suma(int a, int b) {
 	return total;
 }
 
-
-float calculoDebito (float precio){
+float calculoDebito(float precio) {
 	float total;
 
 	total = precio * 0.9;
 	return total;
 }
-float calculoCredito (float precio){
+float calculoCredito(float precio) {
 	float total;
 
-		total = precio * 1.25;
-		return total;
+	total = precio * 1.25;
+	return total;
 }
 
-float calculoBitcoin (float precio){
+float calculoBitcoin(float precio) {
 	float total;
 
-		total = precio * 4606954.55;
-		return total;
+	total = precio / 4606954.55;
+	return total;
 }
 
-float calculoBitcoin (float precio, float km){
+float calculoUnitario(float precio, float km) {
 	float total;
 
-		total = precio/km;
-		return total;
+	total = precio / km;
+	return total;
 }
 
+float diferenciaPrecio(float precioLatam, float precioAero) {
+
+	float total;
+	float diferencia;
+
+	diferencia = precioLatam - precioAero;
+
+	total = abs(diferencia);
+
+	return total;
+}
+
+float valorMostrar(char *mensaje, float valor) {
+
+	printf("%s %f \n", mensaje, valor);
+	return 0;
+}
 
 int utn_getNumero(int *pResultado, char *mensaje, char *mensajeError,
 		int minimo, int maximo, int reintentos) {
@@ -49,53 +65,50 @@ int utn_getNumero(int *pResultado, char *mensaje, char *mensajeError,
 	int bufferInt;
 	if (pResultado != NULL && mensaje != NULL && mensajeError != NULL
 			&& minimo <= maximo && reintentos >= 0) {
-		do{
+		do {
 			printf("%s", mensaje);
-				scanf("%d", &bufferInt);
-				if (bufferInt >= minimo && bufferInt <= maximo) {
+			scanf("%d", &bufferInt);
+			if (bufferInt >= minimo && bufferInt <= maximo) {
 
-					*pResultado = bufferInt;
-					retorno = 0;
-					break;
-				} else {
+				*pResultado = bufferInt;
+				retorno = 0;
+				break;
+			} else {
 
-					printf("%s", mensajeError);
-					reintentos--;
-				}
+				printf("%s", mensajeError);
+				reintentos--;
+			}
 		} while (reintentos >= 0);
 
-
 	}
-
 
 	return retorno;
 
 }
 
-int utn_getNumeroFloat(float* pResultado,char* mensaje,char* mensajeError,float minimo,float maximo,int reintentos) {
+int utn_getNumeroFloat(float *pResultado, char *mensaje, char *mensajeError,
+		float minimo, float maximo, int reintentos) {
 
 	int retorno = -1;
 	int bufferFloat;
 	if (pResultado != NULL && mensaje != NULL && mensajeError != NULL
 			&& minimo <= maximo && reintentos >= 0) {
-		do{
+		do {
 			printf("%s", mensaje);
-				scanf("%d", &bufferFloat);
-				if (bufferFloat >= minimo && bufferFloat <= maximo) {
+			scanf("%d", &bufferFloat);
+			if (bufferFloat >= minimo && bufferFloat <= maximo) {
 
-					*pResultado = bufferFloat;
-					retorno = 0;
-					break;
-				} else {
+				*pResultado = bufferFloat;
+				retorno = 0;
+				break;
+			} else {
 
-					printf("%s", mensajeError);
-					reintentos--;
-				}
+				printf("%s", mensajeError);
+				reintentos--;
+			}
 		} while (reintentos >= 0);
 
-
 	}
-
 
 	return retorno;
 
